51849d85d2e16a9fa336df024bf1b049f83b4dee
oGrandeK <kelvimkauam@gmail.com>
2022-07-07 20:56:18 -0300

    Add files via upload

---
+++ Models/IConsumable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstraindoRPG.Models
+{
+    internal interface IConsumable
+    {
+        public static void UseConsumable(Jogador player)
+        {
+            player.Mana += 0;
+        }
+    }
+}
